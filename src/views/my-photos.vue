b51e0570d0060330bc9183984654ebf56dca588b
ShadowWood <308346420@qq.com>
2016-08-03 21:11:39 +0800

    初始化我的相册页面

--- src/views/my-photos.vue
+++ src/views/my-photos.vue
@@ -1,7 +1,20 @@
 <template>
-    <h1>我的相册</h1>
+    <div class="mdl-cell mdl-cell--12-col photo-board">
+        <div class="mdl-card mdl-shadow--2dp ">
+            <h1>我的相册</h1>
+        </div>    
+    </div>
 </template>
 
 <script>
 
 </script>
+<style>
+    .photo-board {
+        padding: 8px;
+    }
+
+    .photo-board > .mdl-card {
+        width: 100%;
+    }
+</style>
